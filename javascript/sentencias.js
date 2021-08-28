5d30a5ae6f050710042bafa7320f1134b305fb37
Edgarin <maed2802@gmail.com>
2021-08-28 09:49:40 -0500

    ejemplo

---
+++ javascript/sentencias.js
@@ -0,0 +1,57 @@
+/* var numero10 = 10;
+var numero20 = 20;
+
+if(numero10 < numero10){
+    document.write("La variable 20 es menor a la variable 10");
+
+
+} */
+
+//mayor
+/* if (numero10 < numero20) { 
+    
+    document.write ( "la variable 10 es menor a la variable 20");
+    
+} */
+
+/* if (numero2/* 0>numerox) {
+    document.write("La variable 20 es menor a la variable x");
+    
+} */ 
+/* var numero5
+var numero10
+
+if (numero5 <=numero10) {
+    document.write ("verdadero")
+    
+} */
+
+ //sentencia elsei
+ /* var numero10
+ var numero20
+
+if ( numero10> numero20  ) {
+    document.write ("la variable 20 si es mayor a la variable");
+
+} else {
+    document.write("la variable 20 no es mayor a la variable 10")
+} */
+
+
+//var nombre1 = "williams";
+//var nombre2 = "williams";
+
+
+
+    //var edad = 60;
+
+
+    if (edad <= 17  ) {
+        document.write ("eres una jovenazo")
+        
+    } else if (edad >= 18 && edad <= 59) {
+        document.write ("eres un adulto")
+        
+    } else if (edad >= 60) {
+        document.write ("eres un adulto mayor")
+    } 
---
+++ javascript/switch.js
@@ -0,0 +1,22 @@
+var vocal = "C";
+
+switch (vocal) {
+    case "A": 
+        document.write("")
+        break;
+        case "B": 
+        document.write()
+        break;
+        case "C": 
+        document.write("encontre la variable")
+        break;
+        case D: 
+        document.write("")
+        break;
+        case E: 
+        document.write()
+        break;
+
+    default:
+        break;
+}
